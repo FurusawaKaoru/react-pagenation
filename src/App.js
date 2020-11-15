3dd47ef5299380d3bd1aaeababa2e5849ece4795
KaoruFurusawa <kaoru.furusawa@yourmystar.jp>
2020-11-16 07:50:39 +0900

    feature itme-list table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
+
+import { List } from "./List"
 
 function App() {
+  const items = [
+    { title: 'タイトル1', description: '説明テストテスト1' },
+    { title: 'タイトル2', description: '説明テストテスト2' },
+    { title: 'タイトル3', description: '説明テストテスト3' },
+    { title: 'タイトル4', description: '説明テストテスト4' },
+    { title: 'タイトル5', description: '説明テストテスト5' },
+    { title: 'タイトル6', description: '説明テストテスト6' },
+    { title: 'タイトル7', description: '説明テストテスト7' },
+    { title: 'タイトル8', description: '説明テストテスト8' },
+    { title: 'タイトル9', description: '説明テストテスト9' },
+    { title: 'タイトル10', description: '説明テストテスト10' },
+    { title: 'タイトル11', description: '説明テストテスト11' },
+    { title: 'タイトル12', description: '説明テストテスト12' },
+  ]
+
+  const [itemList, setItemList] = useState(items)
+
+  const handlePaginate = (page) => {
+    // APIを叩きに行く処理
+    console.log('ページネーション処理')
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>
+        タイトル
+      </h1>
+      <List itemList={itemList}></List>
+
+      {/* <Pagination
+        sum={100}
+        per={10}
+        onChange={e => handlePaginate(e.page)}
+      /> */}
+    </React.Fragment>
   );
 }
 
-export default App;
+export default App
---
+++ src/List.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+export const List = ({ itemList }) => {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>タイトル</TableCell>
+            <TableCell>説明</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {itemList.map((item) => (
+            <TableRow key={item.title}>
+              <TableCell component="th" scope="row">
+                {item.title}
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {item.description}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
